8ecb7abd9e622bb185ca4dd8602ea6eeaf74bc14
woogod <aqaok12@gmail.com>
2020-08-12 23:36:25 +0900

    protect info

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyPaser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyPaser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyPaser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://woogod:eprkt1a@boilerplate.x4dvk.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!asfd'))
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if (err) return res.json({ success: false, err })
+      return res.status(200).json({
+        success: true
+      })
+    }) 
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
